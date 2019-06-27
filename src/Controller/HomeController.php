b70cbef5a068180eb338f8610b3fca2cff049677
Pascal Encinas <krionari@gmail.com>
2019-06-27 22:58:44 +0200

    creation controller

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Krionari
+ * Date: 27/06/2019
+ * Time: 22:03
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="index")
+     * @return Response A response instance
+     */
+        public function index(): Response
+        {
+            return $this->render('home/index.html.twig');
+        }
+
+}
---
+++ src/Controller/QuestionController.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Krionari
+ * Date: 27/06/2019
+ * Time: 22:03
+ */
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class QuestionController extends AbstractController
+{
+    /**
+     * @Route("/question/{id}", name="question_index")
+     * @return Response A response instance
+     */
+        public function index(): Response
+        {
+            return $this->render('question/index.html.twig');
+        }
+
+    /**
+     * @Route("/solution/{id}", name="question_index")
+     * @return Response A response instance
+     */
+    public function solution(): Response
+    {
+        return $this->render('question/solution.html.twig');
+    }
+
+}
